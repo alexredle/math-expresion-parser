175ae0e70eac864a23f710f8952245956d87947a
elderprado <elderprado@192.168.1.15>
2020-09-04 21:17:39 -0600

    INI: Base para taller Jlex y cup

---
+++ src/main/java/gt/edu/umg/alf/Main.java
@@ -0,0 +1,16 @@
+package gt.edu.umg.alf;
+
+/**
+ *
+ * @author elderprado
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
